612fd13a4d6f68b7d1a947ce48139517f8241fe3
youuuuuli <andyyant2012@gmail.com>
2023-03-24 11:24:12 +0800

    install element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<script setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const locale = ref(zhCn)
+</script>
 
 <template>
-  <header>header</header>
-
-  <main>main</main>
+  <el-config-provider :locale="locale">
+    <el-container>
+      <el-header> Header </el-header>
+      <el-main> Main </el-main>
+    </el-container>
+  </el-config-provider>
 </template>
 
 <style scoped></style>
